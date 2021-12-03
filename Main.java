11e6a307f2c4ab786cadf6694795ad4d0cf8d924
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-03 10:23:51 +0900

    Lesson1_3

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.Map;
 class Main{
