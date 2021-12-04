c332d014e34011b891815c416d6c965bcde35933
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-04 18:06:13 +0900

    Lesson2_6

---
+++ Lesson2/Aseet.java
@@ -0,0 +1,12 @@
+public abstract class Asset{
+  String name;
+  int price;
+
+  public TangibleAsset(String name, int price){
+    this.name = name;
+    this.price = price;
+  }
+
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+}
