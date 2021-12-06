e46a48e46c8d6b098b2a555a1aaa1791c41c6bf3
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-06 11:01:54 +0900

    Lesson3_3

---
+++ Lesson3/Lesson3_3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Lesson3_3{
+  public static void main(String[] args){
+    Hero h1 = new Hero("斉藤");
+    Hero h2 = new Hero("鈴木");
+    Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
+    heroes.put(h1, 3);
+    heroes.put(h2, 7);
+    for (Hero key : heroes.keySet()){
+      int value = heroes.get(key);
+      System.out.println(key.getName() + "が倒した敵=" + value);
+    }
+  }
+}
