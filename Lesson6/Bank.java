95d7565b69fafaeb716cdaa256dcfdc611282772
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-14 16:17:36 +0900

    Lesson6_1

---
+++ Lesson6/Bank.java
@@ -0,0 +1,20 @@
+package jp.miyabilink.atm;
+/**
+ * 銀行を表すクラス
+ */
+public class Bank{
+  /** 銀行名 */
+  String name;
+  /** 銀行の住所 */
+  String address;
+  /**
+   * 口座追加メソッド
+   * @param owner 口座の名義人
+   * @param initialBalance 初期残高
+   */
+  public void addAccount(String owner, int initialBalance){}
+
+  public static void main(String[] args){
+    System.out.println("試験用のメインメソッドです");
+  }
+}
