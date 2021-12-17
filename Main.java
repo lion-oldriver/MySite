c443df275f3223f98ecb826de3e334d61eb5b894
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-17 16:14:20 +0900

    Exercises

--- Main.java
+++ Main.java
@@ -1,18 +1,38 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.io.*;
+
 class Main{
   public static void main(String[] args){
-    HashMap<String,Integer> map = new HashMap<>();
+    try{
+      FileReader fr = new FileReader(args[0]);
+      BufferedReader br = new BufferedReader(fr);
+      String line;
+      Map<String, String> pref = new TreeMap<String, String>(new NameComparator());
+      while((line = br.readLine()) != null){
+        String[] list = line.split(" ");
+        pref.put(list[0], list[1]);
+      }
+      br.close();
+      for(String key : pref.keySet()){
+        String value = pref.get(key);
+        System.out.println(key + " : " + value);
+      }
+    }catch(FileNotFoundException e){
+      e.printStackTrace();
+    }catch(IOException e2){
+      e2.printStackTrace();
+    }
+  }
+}
 
-    map.put("リンゴ",80);
-    map.put("オレンジ",120);
-    map.put("ブドウ",90);
-
-    System.out.println(map.get("リンゴ"));
-    map.putIfAbsent("メロン",140);
-    System.out.println(map.containsValue(120));
+class NameComparator implements Comparator<String>{
+  @Override
+  public int compare(String o1, String o2){
+    if(o1.length() > o2.length()){
+      return 1;
+    }else if(o1.length() < o2.length()){
+      return -1;
+    }
+    return o1.compareTo(o2);
   }
 }
