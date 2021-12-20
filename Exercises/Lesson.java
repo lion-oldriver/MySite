6fd67148c9f009497b1986cd4d484814185162fa
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-20 14:41:05 +0900

    exercises

--- Exercises/Lesson.java
+++ Exercises/Lesson.java
@@ -1,18 +1,26 @@
 public class Lesson {
   public static void main(String[] args){
-    int[] arr = {43, 2, 765, 234, 908, 115, 32,
-                 476, 938, 65, 93, 47, 102, 735,
-                 529, 1010, 639, 660, 182, 908};
-    System.out.println("配列の最大:" + max(arr));
-  }
+    String[][] apartment = {
+      {"後藤", "矢口", "石川", "吉澤", "藤本"},
+      {"亀井", "小川", "紺野"},
+      {"石川", "安部", "安田", "後藤"},
+      {"石黒", "福田", "後藤"}
+    };
 
-  static int max(int[] arr){
-    int max = 0;
-    for(int i : arr){
-      if(max < i){
-        max = i;
+    System.out.println("検索する名前を入力してください");
+    String name = System.console().readLine();
+    int count = 1;
+
+    for(int i=0; i<apartment.length; i++){
+      for(int j=0; j<apartment[i].length; j++){
+        if(name.equals(apartment[i][j])){
+          System.out.println(count + "人目の" + name + "さんは" + (i + 1) + "階の" + (j + 1) + "号室に住んでいます");
+          count++;
+        }
       }
     }
-    return max;
+    if(count == 1){
+      System.out.println(name + "さんはこのアパートに住んでいません");
+    }
   }
 }
